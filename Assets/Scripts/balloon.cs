c1a4bdef40f87df6d8bd7d31ea16a6c864798167
Clovis Gauthier <e2290836@cmaisonneuve.qc.ca>
2024-10-22 11:18:43 -0400

    ballons respawn canvas score en cours

--- Assets/Scripts/balloon.cs
+++ Assets/Scripts/balloon.cs
@@ -5,11 +5,23 @@ using Oculus.Interaction;
 
 public class balloon : MonoBehaviour
 {
+    float scoreLocal = GameManager.score;
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.name == "PikePike")
         {
-            Destroy(gameObject);
+            // Désactive le ballon
+            gameObject.SetActive(false);
+            // incrémente le score
+            scoreLocal+= 1;
+            GameManager.score = scoreLocal;
+            print(GameManager.score);
+            // actualise le canvas
+            GameManager.ActualiserScore();
         }
+    }
+    void Update()
+    {
+        //print(scoreLocal);
     }
 }
